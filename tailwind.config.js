54d8f6ddc9c21b687f18ab8daa16051964bf8cb5
karlebh <akejucaleb@gmail.com>
2022-04-02 21:09:20 +0100

    Darkmode bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,13 @@ module.exports = {
   purge: [
   "./index.html",
   "./src/**/*.{vue,js,ts,jsx,tsx}",
+  "./src/**/**/*.{vue,js,ts,jsx,tsx}",
   ],
   darkMode: 'class',
   content: [
   "./index.html",
   "./src/**/*.{vue,js,ts,jsx,tsx}",
+  "./src/**/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
